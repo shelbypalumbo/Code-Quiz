3df1818023fe2b9b43af4fbffd040791333d919d
shelby palumbo <shelbypalumbo94@gmail.com>
2019-12-20 19:27:18 -0500

    Added comments in questions.js ffile.

--- js/questions.js
+++ js/questions.js
@@ -27,30 +27,22 @@ var questions = [
       },
   ];
   
- 
+ //Timer variables
 var score = 0;
 var timeEl = document.querySelector(".time");
 var secondsLeft = 75;
 
+//Question variables
 var questionBox = document.getElementById("questionText");
 var choiceA = document.getElementById("abtn");
 var choiceB = document.getElementById("bbtn");
 var choiceC = document.getElementById("cbtn");
 var choiceD = document.getElementById("dbtn");
 
-questionBox.textContent=(questions[0]["title"]);
-
-choiceA.textContent=(questions[0]["choices"][0]);
-
-choiceB.textContent=(questions[0]["choices"][1]);
-
-choiceC.textContent=(questions[0]["choices"][2]);
-
-choiceD.textContent=(questions[0]["choices"][3]);
+document.getElementById("submit").addEventListener("click", checkAns);
 
 
-
-
+//Start button calls the timer and question functions
 function questAndTime(){
   setTime();
   sendMessage();
@@ -58,19 +50,20 @@ function questAndTime(){
 };
 
 
+//Provices the content of the questions
 function quizQuestions(){
   for ( var i = 0; i < questions.length; i++ ){
-    for (item of questions[i]) {
-   questionBox.textContent = (questions[i]);
-   questionBox.append(questions[0]);
-    }
+    questionBox.textContent=(questions[i]["title"]);
+  choiceA.textContent=(questions[i]["choices"][0]);
+  choiceB.textContent=(questions[i]["choices"][1]);
+  choiceC.textContent=(questions[i]["choices"][2]);
+  choiceD.textContent=(questions[i]["choices"][3]);
   }
 }
-    
 
-
-function checkAnsOne(){
-  if( choiceC === clicked ){ 
+//Check answer function
+function checkAns(){
+  if ( questions[i]["answer"[0]] === clicked ){ 
     alert("Correct!");
   } else {
       alert("Incorrect");
@@ -78,6 +71,7 @@ function checkAnsOne(){
   }
 }
 
+//
   function nextQuestion(){
     if( answer === clicked ){ 
       alert("Correct!");
@@ -88,15 +82,13 @@ function checkAnsOne(){
   }
 
   
-
-
-
+//If the answer is wrong this function is called
 function wrongAns(){
-  timeEl.textContent = secondLeft - 15;
+  timeEl.textContent = (secondLeft - 15);
 }
 
 
-
+//--------------------------------TIMER---------------------------------------------------------
 function setTime() {
    var timerInterval = setInterval(function() {
     secondsLeft--;
